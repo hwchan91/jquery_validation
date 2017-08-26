7add7ffd52a03399162c1c6511412f4cc0ac4cb6
hwchan91 <hwchan91@gmail.com>
2017-08-26 12:53:52 +0800

    refined self_defined_validation

--- js/self_defined_validator.js
+++ js/self_defined_validator.js
@@ -1,21 +1,21 @@
 $ (function(){
   var validation = {
     check_email: function() {
-      $('#email').blur(function(){
+//      $('#email').blur(function(){
         var email = $('#email').val()
         var email_valid = /.+@.+\..+/.test(email)
         validation.toggleError.call($('#email'), email_valid, "email", "Invalid email");
         if (email_valid) { validation.equal( $('#email'), $('#email_confirm'), "email_confirm", "Email must be equal"); } 
-        });      
+//        });      
     },
     
     check_password: function() {
-      $('#password').blur(function(){
+//      $('#password').blur(function(){
         var pw = $('#password').val()
         var pw_valid = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d@#$%^&*\.\!]{6,30}$/.test(pw)
         validation.toggleError.call($('#password'), pw_valid, "password", "Password must be within 6-30 characters, contain a small letter, a captial letter, and a number");
         if (pw_valid) { validation.equal( $('#password'), $('#password_confirm'), "password_confirm", "Password must be equal"); } 
-      })
+//      })
     },
     
     toggleError: function(criteria, name, msg) {
@@ -31,39 +31,56 @@ $ (function(){
     },
     
     equal: function(elem1, elem2, name, msg) {
-      elem2.blur(function(){
+//      elem2.blur(function(){
         var input1 = elem1.val(),
             input2 = elem2.val()
         var same_inputs = input1 == input2
         if (input2 != "") {
           validation.toggleError.call(elem2, same_inputs, name, msg);
         }
-      })
+//      })
     },
 
     
     submit: function() {
       $('#form').on('submit', function(event){
         event.preventDefault();
-        alert("Form submitted");
+	validation.check_all();
+	submitted_once = true
+	if ($('div').hasClass('error')) {
+	  alert("Form has errors");
+	} else {
+          alert("Form submitted");
+	}
       })
     },
     
     require: function(element, name) {
-      element.blur(function() {
+ //     element.blur(function() {
         var empty = element.val() == ""
         validation.toggleError.call(element, !empty, name, "Field is required");      
-      })
+ //     })
+    },
+
+    check_all: function() {
+	  validation.require($('#country'), "country");
+	  validation.require($('#email_confirm'), "email_confirm");
+	  validation.require($('#password_confirm'), "password_confirm");
+	  validation.check_email();
+	  validation.check_password();
     }
 
   }
-  validation.require($('#country'), "country");
-  validation.require($('#email_confirm'), "email_confirm");
-  validation.require($('#password_confirm'), "password_confirm");
-  validation.check_email();
-  //validation.equal( $('#email'), $('#email_confirm'), "email_confirm", "Different email");
-  validation.check_password();
+
+
   validation.submit();
+  var submitted_once = false;
+    $('input').on('focus keydown keyup', function(){
+      if (submitted_once) {
+        validation.check_all();
+      }
+    });
+
 
 })
 
